ccf2092fb9ceb4a7609c1bb76c14aeb3712a0604
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-08-25 11:36:53 +0200

    Version bump to 1.0.0
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- kaoz/__init__.py
+++ kaoz/__init__.py
@@ -2,5 +2,5 @@
 # Copyright © 2011-2012 Binet Réseau
 # See the LICENCE file for more informations
 
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 __author__ = "Binet Réseau <br@eleves.polytechnique.fr>"
