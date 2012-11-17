fb7d9308b4104fda35c21f626f2e0262b1dffd78
Nicolas Iooss <nicolas.iooss+git@polytechnique.org>
2012-11-17 17:25:57 +0100

    Version bump to 1.1.1
    Signed-off-by: Nicolas Iooss <nicolas.iooss+git@polytechnique.org>

--- kaoz/__init__.py
+++ kaoz/__init__.py
@@ -2,5 +2,5 @@
 # Copyright © 2011-2012 Binet Réseau
 # See the LICENCE file for more informations
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 __author__ = "Binet Réseau <br@eleves.polytechnique.fr>"
