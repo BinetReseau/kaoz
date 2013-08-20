45686590f6163ec5d98fdc068b4fedf67c9394dd
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2013-08-20 12:37:16 +0200

    Release v2.0.3
    Signed-off-by: Nicolas Iooss <nicolas.iooss_git@polytechnique.org>

--- kaoz/__init__.py
+++ kaoz/__init__.py
@@ -2,5 +2,5 @@
 # Copyright © 2011-2013 Binet Réseau
 # See the LICENCE file for more informations
 
-__version__ = '2.0.2'
+__version__ = '2.0.3'
 __author__ = "Binet Réseau <br@eleves.polytechnique.fr>"
