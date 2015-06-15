f134f3dbef097c295788ae4624b526e8f09c098e
Guillaume DIDIER <guillaume.didier95@hotmail.fr>
2015-06-15 22:24:08 +0200

    Release v2.2.0

--- kaoz/__init__.py
+++ kaoz/__init__.py
@@ -2,5 +2,5 @@
 # Copyright © 2011-2013 Binet Réseau
 # See the LICENCE file for more informations
 
-__version__ = '2.1.0'
+__version__ = '2.2.0'
 __author__ = "Binet Réseau <br@eleves.polytechnique.fr>"
